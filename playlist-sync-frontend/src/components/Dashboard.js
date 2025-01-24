7f00041801d0a81346509b8f3a23c8a8b46dd6af
Tharindu H <tharindudeshan500@gmail.com>
2025-01-23 23:44:05 -0500

    Use AuthContext in Dashboard and check which platform is logged in

--- playlist-sync-frontend/src/components/Dashboard.js
+++ playlist-sync-frontend/src/components/Dashboard.js
@@ -1,21 +1,31 @@
 import React from "react";
-import SpotifyDashboard from "./SpotifyDashboard"; // Spotify Dashboard
-import AppleMusicDashboard from "./AppleMusicDashboard"; // Apple Music Dashboard
+import SpotifyDashboard from "./SpotifyDashboard"; 
+import AppleMusicDashboard from "./AppleMusicDashboard";
+import HomePage from "./HomePage";
+import { useAuth } from "../context/AuthContext"; 
 
 function Dashboard() {
-    return (
-        <div className="min-h-screen flex bg-gray-100">
-            {/* Spotify Dashboard */}
-            <div className="w-1/2 border-r border-gray-300">
-                <SpotifyDashboard />
-            </div>
-
-            {/* Apple Music Dashboard */}
-            <div className="w-1/2">
-                <AppleMusicDashboard />
-            </div>
-        </div>
-    );
+  const { isAppleMusicLoggedIn, isSpotifyLoggedIn } = useAuth();
+  return (
+    <div className="min-h-screen flex bg-gray-100">
+      <div className="w-1/2 border-r border-gray-300">
+        {isSpotifyLoggedIn ? (
+          <SpotifyDashboard />
+        ) :
+        (
+          <HomePage showSpotifyLogin={true}/>
+        )}
+        
+      </div>
+      <div className="w-1/2">
+        {isAppleMusicLoggedIn ? (
+          <AppleMusicDashboard />
+        ) : (
+          <HomePage showAppleMusicLogin={true} />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
